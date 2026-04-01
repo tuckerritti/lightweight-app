c9aae468a8c3d5294ac14876f1a4b7d94b9967c8
Tucker Ritti <45155694+tuckerritti@users.noreply.github.com>
2026-04-01 17:15:34 -0400

    Disable iCloud backup in debug builds

--- light-weight/ContentView.swift
+++ light-weight/ContentView.swift
@@ -77,9 +77,7 @@ struct ContentView: View {
             Color(hex: 0x0A0A0A)
                 .ignoresSafeArea()
                 .task {
-                    #if !DEBUG
                     await ICloudBackupService.restoreIfNeeded(modelContext: modelContext)
-                    #endif
                     isRestoring = false
                 }
         } else if needsOnboarding {
